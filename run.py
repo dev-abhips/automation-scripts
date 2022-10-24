c38c82b71ebd7ea4caefb8576461389d6075fbbb
Abhilash PS <developer.abhilash@gmail.com>
2022-10-24 23:00:50 +0530

    Menu function
    Auto generate the menu from the Choice Enum

--- run.py
+++ run.py
@@ -2,28 +2,30 @@ from enum import Enum
 
 import typer
 from tasks.duplicate_detector_task import DuplicateDetectorTask
-from tasks.downloads_cleanup import DirectoryCleanupTask
+from tasks.directory_cleanup import DirectoryCleanupTask
 
 
 class Choice(Enum):
     MANIPULATE_DUPLICATE = 1
     CLEANUP_DIRECTORY = 2
 
+    def get_menu():
+        print("\n------------ Menu ------------\n")
+        menu: dict = {}
+        for name, entry in Choice.__members__.items():
+            print(f"{entry.value}) {name.replace('_', ' ').capitalize()}.")
+            menu.update({entry.value: name})
+        print("\n")
+        return menu
+
 
 def main() -> None:
-    """
-    Manage duplicates Task: Detect the duplicate files in a directory and
-    notify the user.
-    """
-    print("\n------------ Menu ------------\n")
-    print("1) Manage duplicates")
-    print("2) Cleanup downloads")
-    choice = int(input("Enter the choice of action: "))
-    if choice == Choice.MANIPULATE_DUPLICATE.value:
-        print("\nManaging duplicates.")
+    choices: dict = Choice.get_menu()
+    menu_choice = int(input("Enter the choice of action: "))
+    print(f"\nYou have chosen the task `{choices.get(menu_choice).replace('_', ' ').capitalize()}`.")
+    if menu_choice == Choice.MANIPULATE_DUPLICATE.value:
         task = DuplicateDetectorTask()
-    elif choice == Choice.CLEANUP_DIRECTORY.value:
-        print("\nCleanup Downloads.")
+    elif menu_choice == Choice.CLEANUP_DIRECTORY.value:
         task = DirectoryCleanupTask()
     task.run()
     print("\n------------------------------\n")
