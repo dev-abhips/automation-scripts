01b55b0a26e2bf39fc7416852575a1a74763e76e
Abhilash PS <developer.abhilash@gmail.com>
2022-10-27 01:08:06 +0530

    Rename `directory_cleanup.py` to `directory_cleanup_task.py`

--- tasks/task.py
+++ tasks/task.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from tasks.base_task import BaseTask
-from tasks.directory_cleanup import DirectoryCleanupTask
+from tasks.directory_cleanup_task import DirectoryCleanupTask
 from tasks.duplicate_detector_task import DuplicateDetectorTask
 
 
